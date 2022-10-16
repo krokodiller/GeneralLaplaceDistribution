aa38a4d15e3b26f45b5804f6bc8c0402c132468c
Alexander Izotov <75754617+krokodiller@users.noreply.github.com>
2022-10-17 02:33:58 +0700

    removed match-case from python script

--- Laplas/graph.py
+++ Laplas/graph.py
@@ -13,77 +13,77 @@ for gr in graphs:
 
 graph = graphs[int(input())]
 
-match graph:
-    case "density+accurate random histogram":
-        f = open("density.txt")
-        for line in f:
-            data = line.split(" ")
-            a.append(float(data[0]))
-            b.append(float(data[1].strip("\n")))
-        f.close()
 
-        r = open("random.txt")
-        for line in r:
-            if math.isinf(float(line)):
-                continue
-            if math.isnan(float(line)):
-                continue
-            random.append(float(line))
-        r.close()
+if graph == "density+accurate random histogram":
+    f = open("density.txt")
+    for line in f:
+        data = line.split(" ")
+        a.append(float(data[0]))
+        b.append(float(data[1].strip("\n")))
+    f.close()
 
-        plt.hist(random, density=True, bins=int(len(random) / (len(random) / 100)), histtype=u'step')
-        plt.plot(a, b)
-        plt.title("Density and Accurate random values")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend(['Accurate random values', 'Density'])
-        plt.show()
-    case "density+distorted random histogram":
-        f = open("density.txt")
-        for line in f:
-            data = line.split(" ")
-            a.append(float(data[0]))
-            b.append(float(data[1].strip("\n")))
-        f.close()
+    r = open("random.txt")
+    for line in r:
+        if math.isinf(float(line)):
+            continue
+        if math.isnan(float(line)):
+            continue
+        random.append(float(line))
+    r.close()
 
-        r = open("distRandom.txt")
-        for line in r:
-            if math.isinf(float(line)):
-                continue
-            if math.isnan(float(line)):
-                continue
-            random.append(float(line))
-        r.close()
+    plt.hist(random, density=True, bins=int(len(random) / (len(random) / 100)), histtype=u'step')
+    plt.plot(a, b)
+    plt.title("Density and Accurate random values")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend(['Accurate random values', 'Density'])
+    plt.show()
+elif graph == "density+distorted random histogram":
+    f = open("density.txt")
+    for line in f:
+        data = line.split(" ")
+        a.append(float(data[0]))
+        b.append(float(data[1].strip("\n")))
+    f.close()
 
-        plt.hist(random, density=True, bins=int(len(random) / (len(random) / 100)), histtype=u'step')
-        plt.plot(a, b)
-        plt.title("Density and Distorted random values")
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend(['Distorted random values', 'Density'])
-        plt.show()
-    case "accurate random":
-        r = open("random.txt")
-        for line in r:
-            if math.isinf(float(line)):
-                continue
-            if math.isnan(float(line)):
-                continue
-            random.append(float(line))
-        r.close()
+    r = open("distRandom.txt")
+    for line in r:
+        if math.isinf(float(line)):
+            continue
+        if math.isnan(float(line)):
+            continue
+        random.append(float(line))
+    r.close()
 
-        plt.plot(random, u'm.', markersize=1)
-        plt.show()
-    case "distorted random":
-        r = open("distRandom.txt")
-        for line in r:
-            if math.isinf(float(line)):
-                continue
-            if math.isnan(float(line)):
-                continue
-            random.append(float(line))
-        r.close()
+    plt.hist(random, density=True, bins=int(len(random) / (len(random) / 100)), histtype=u'step')
+    plt.plot(a, b)
+    plt.title("Density and Distorted random values")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend(['Distorted random values', 'Density'])
+    plt.show()
+elif graph ==  "accurate random":
+    r = open("random.txt")
+    for line in r:
+        if math.isinf(float(line)):
+            continue
+        if math.isnan(float(line)):
+            continue
+        random.append(float(line))
+    r.close()
 
-        plt.plot(random, u'm.', markersize=1)
-        plt.show()
+    plt.plot(random, u'm.', markersize=1)
+    plt.show()
+elif graph ==  "distorted random":
+    r = open("distRandom.txt")
+    for line in r:
+        if math.isinf(float(line)):
+            continue
+        if math.isnan(float(line)):
+            continue
+        random.append(float(line))
+    r.close()
+
+    plt.plot(random, u'm.', markersize=1)
+    plt.show()
 
